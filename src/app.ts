c63304e682864ecba4df42d1b887f1293ed18a13
Pedro Fontes <pedro7fontes@gmail.com>
2022-09-08 23:19:46 -0300

    feat: add app.ts

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import "express-async-errors";
+import express from "express";
+import routes from "./routes";
+import { errorMiddleware } from "./middlewares/error";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use(errorMiddleware);
+
+export default app;
